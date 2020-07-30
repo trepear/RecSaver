ff60ad6adc3a9be976f9cec3c09c1c28dbac99a9
trepear <tbuhrsmith@gmail.com>
2020-07-30 11:58:13 -0500

    styling

--- public/js/list.js
+++ public/js/list.js
@@ -12,36 +12,36 @@ const getMovies = () => {
                 let body = movie[i].body;
                 let id = movie[i].id;
                 
-                var movieTitleDiv = $("<div>").attr("class",".card-title");
-                var movieNoteDiv = $("<div>").attr("class",".card-body");
+                var movieTitleDiv = $("<div>").attr("class","card-header");
+                var movieNoteDiv = $("<div>").attr("class","card-body");
 
+                
+
+            // add text box to save-titles div // grab value for id // concat 
+                // var completeBtn = $("<button>").attr("class","complete").text("Mark as watched");
+                // $(movieTitleDiv).append(completeBtn)
+
+                if (body === null || ""){
+                    var titleText = $("<h2>").text(title);
+                    $(movieTitleDiv).append(titleText);
+                    $("#saved-titles").append(movieTitleDiv);
+                     
+                } else {
+                    var titleText = $("<h2>").text(title);
+                    var bodyText = $("<input>").val(body);
+                    console.log("in loop",bodyText.val());
+                    
+                   $(movieTitleDiv).append(titleText); 
+                   $(movieNoteDiv).append(bodyText); 
+                   
+                   $(movieTitleDiv).append(movieNoteDiv);
+                   $("#saved-titles").append(movieTitleDiv);
+                }
                 var deleteBtn = $("<button>").attr({"class":"delete","id":"delete-"+id,"value":id}).text("Delete a movie");
                 $(movieTitleDiv).append(deleteBtn)
 
                 var updateBtn = $("<button>").attr({"class":"update","id":"update-"+id,"value":id}).text("Add or update a note");
                 $(movieTitleDiv).append(updateBtn)
-
-            // add text box to save-titles div // grab value for id // concat 
-                var completeBtn = $("<button>").attr("class","complete").text("Mark as watched");
-                $(movieTitleDiv).append(completeBtn)
-
-                if (body === null || ""){
-                    var titleText = $("<h3>").text(title);
-                    $(movieTitleDiv).append(titleText);
-                    $("#saved-titles").append(movieTitleDiv);
-                     
-                } else {
-                    var titleText = $("<h3>").text(title);
-                    var bodyText = $("<input>").val(body);
-                    console.log("in loop",bodyText.val());
-                    
-                   $(movieTitleDiv).append(titleText); 
-                   $("#saved-titles").append(bodyText); 
-                   
-                   $(movieTitleDiv).append(movieNoteDiv);
-                   $("#saved-titles").append(movieTitleDiv);
-                }
-                
             }
             // DELETE ROUTE
             $('#saved-titles').on('click', '.delete', function() {
--- public/js/search.js
+++ public/js/search.js
@@ -15,30 +15,31 @@ $(document).ready(function () {
                 console.log(movie);
                 let html =
 `
-<div id="content-goes-here">
+
+
+<div class="card mb-3" style="max-width: 540px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="${movie.Poster}" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${movie.Title}</h5>
             <p>
-                Title: <span id="content-title">${movie.Title}</span>
+                <span id="content-plot">${movie.Plot}</span>
             </p>
-            <p>
-                <img src="${movie.Poster}" alt="Movie Poster"/>    
-            </p>
-            <p>
-                Release Year: <span id="content-year">${movie.Year}</span>
-            </p>
-            <p>
-                Rating: <span id="content-rating">${movie.Rated}</span>
-            </p>
-            <p>
-                Plot: <span id="content-plot">${movie.Plot}</span>
-            </p>
-            <p>
-                Runtime: <span id="content-runtime">${movie.Runtime}</span>
-            </p>
-            <p>
-                imdb Rating: <span id="content-runtime">${movie.imdbRating}</span>
-            </p>
-            
-</div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><span id="content-year">${movie.Year}</span></li>
+                <li class="list-group-item"><span id="content-rating">${movie.Rated}</span></li>
+                <li class="list-group-item"><span id="content-runtime">${movie.Runtime}</span></li>
+                <li class="list-group-item"><span id="content-runtime">${movie.imdbRating}</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 `
 $('#content-goes-here').html(html)
 
@@ -78,3 +79,18 @@ $('#content-goes-here').html(html)
 })
 
 
+// <div class="card" style="width: 18rem;" id="content-goes-here">
+// <img class="card-img-top" src="${movie.Poster}" alt=""/>
+// <div class="card-body">
+// <h5 id="content-title" class="card-title">${movie.Title}</h5>
+// <p>
+//     <span id="content-plot">${movie.Plot}</span>
+// </p>
+// </div>   
+// <ul class="list-group list-group-flush">
+//     <li class="list-group-item"><span id="content-year">${movie.Year}</span></li>
+//     <li class="list-group-item"><span id="content-rating">${movie.Rated}</span></li>
+//     <li class="list-group-item"><span id="content-runtime">${movie.Runtime}</span></li>
+//     <li class="list-group-item"><span id="content-runtime">${movie.imdbRating}</span></li>
+// </ul>
+// </div>
